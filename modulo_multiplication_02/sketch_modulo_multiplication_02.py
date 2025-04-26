3a18a0701b38312db5cb9a09c490004a9baeba33
bastian <bastianra@gmail.com>
2025-04-26 20:26:04 +0200

    bug fix for circles draw multiple times. Added outputs.

--- modulo_multiplication_02/sketch_modulo_multiplication_02.py
+++ modulo_multiplication_02/sketch_modulo_multiplication_02.py
@@ -1,21 +1,33 @@
 import vsketch
 import math
 
+class Point:
+  def __init__(self,x,y):
+      self.x = x
+      self.y = y
+  def __eq__(self, value):
+      d_sq = (self.x-value.x)**2 + (self.y-value.y)**2
+      return(d_sq < 1e-20)
+  def dist(self,value):
+     return math.sqrt((self.x-value.x)**2 + (self.y-value.y)**2)
+
+  def  __repr__(self):
+     return f'({self.x},{self.y})'
+   
+ 
 def crossProduct2d(p1x,p1y, p2x,p2y) :
     return p1x * p2y - p1y * p2x
 
-def dist(p1,p2):
-   return math.sqrt( (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
 
 def minDist(ref,points):
-   dists = [dist(ref,x) for x in points ]
+   dists = [ref.dist(x) for x in points ]
    dists = list(filter(lambda a: a > 1e-10, dists))
    return min(dists)
 
 # assume no points are equal and line segments cannot lie on the same line
 def intersect(line1,line2) :
   if line1==line2:
-     return [float('inf'),float('inf')]  
+     return Point(float('inf'),float('inf'))
   x1 = line1[0]
   y1 = line1[1]
   x2 = line1[2]
@@ -27,13 +39,13 @@ def intersect(line1,line2) :
   y4 = line2[3]
 
   if (x1==x3) and (y1==y3):
-    return [float('inf'),float('inf')]
+    return Point(float('inf'),float('inf'))
   if (x1==x4) and (y1==y4):
-    return [float('inf'),float('inf')]
+    return Point(float('inf'),float('inf'))
   if (x2==x3) and (y2==y3):
-    return [float('inf'),float('inf')]
+    return Point(float('inf'),float('inf'))
   if (x2==x4) and (y2==y4):
-    return [float('inf'),float('inf')]
+    return Point(float('inf'),float('inf'))
 
   rx = x2-x1
   ry = y2-y1
@@ -43,7 +55,7 @@ def intersect(line1,line2) :
   denominator = crossProduct2d(rx,ry, sx,sy)
 
   if (denominator == 0) :
-    return [float('inf'),float('inf')]
+    return Point(float('inf'),float('inf'))
 
   qpx = x3-x1
   qpy = y3-y1
@@ -51,9 +63,9 @@ def intersect(line1,line2) :
   t = crossProduct2d(qpx,qpy, sx,sy) / denominator
 
   if ((t >= 0) and (t <= 1) and (u >= 0) and (u <= 1)) :
-    return[x1+t*rx,y1+t*ry]
+    return Point(x1+t*rx,y1+t*ry)
   else :
-    return [float('inf'),float('inf')]
+    return Point(float('inf'),float('inf'))
   
 
 
@@ -76,13 +88,16 @@ class ModuloMultiplication01Sketch(vsketch.SketchClass):
         
  
         intersections = [intersect(l1,l2) for l1 in lines for l2 in lines]
-        intersections[:] = [x for x in intersections if x[0] < float('inf')]
+        intersections[:] = [x for x in intersections if x.x < float('inf')]
         intersections = [ii for n,ii in enumerate(intersections) if ii not in intersections[:n]]
-        
-        nn = [minDist(p,intersections) for p in intersections]
-
+   
+        circle = [Point(-self.r * math.cos(i*tpn), self.r * math.sin(i * tpn)) for i in range(min(1000,self.n))]
+        nn = [minDist(p,intersections + circle) for p in intersections]
+        index =0
+        print(str(len(nn)))
         for i,d in zip(intersections,nn):
-           vsk.circle(i[0], i[1], d)
+           vsk.circle(i.x, i.y, d)
+           index +=1
         vsk.text(text = f'n={self.n}',x=-self.r,y=self.r,size = "0.25",mode = "transform",font = "futural")
         vsk.text(text = f'x={self.multiplier}',x=self.r,y=self.r,size = "0.25",mode = "transform",font = "futural",align="right")
 
