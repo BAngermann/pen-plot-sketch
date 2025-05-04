92ea5a1beb5c2471cbd13afdd549501d75c82c90
bastian <bastianra@gmail.com>
2025-05-04 17:30:12 +0200

    added line thickness and global scaling options

--- Takagi/sketch_takagi.py
+++ Takagi/sketch_takagi.py
@@ -1,4 +1,5 @@
 import vsketch
+import math
 
 def s_fun(x,n,w):
     x = x * 2**n
@@ -9,14 +10,15 @@ class TakagiSketch(vsketch.SketchClass):
     scale = vsketch.Param(0.6)
     n_max = vsketch.Param(5)
     penWidth = vsketch.Param(0.2,decimals=2)
+    stroke_iteration_scale = vsketch.Param(1,min_value =1, max_value=9)
     w = vsketch.Param(0.5,decimals=3,min_value=0,max_value=1)
+    page_scale = vsketch.Param(12,decimals=1,min_value = 0.1,max_value=20)
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
         vsk.size("a4", landscape=False)
         vsk.scale("cm")
         vsk.pushMatrix()
-        page_scale = 12
-        vsk.scale(page_scale)
+        vsk.scale(self.page_scale)
         
         vsk.penWidth(f'{self.penWidth}mm')
          
@@ -27,13 +29,13 @@ class TakagiSketch(vsketch.SketchClass):
             y_add = [-s_fun(x,n,self.w) for x,y in zip(x_points,y)]
 
             y_plot = [y + self.scale*y_1 for y,y_1 in zip(y,y_add)]
-            stroke = self.n_max+1-n
+            stroke = math.ceil((self.n_max+1.-n)/self.stroke_iteration_scale)
 
             vsk.strokeWeight(stroke)
             for i in range(len(x_points)):
                 vsk.line(x_points[i],y[i],x_points[i],y_plot[i])
             y = [y + y_1 for y,y_1 in zip(y,y_add)]
-            vsk.translate(0,-stroke*self.penWidth/10/page_scale)   
+            vsk.translate(0,-stroke*self.penWidth/10/self.page_scale)   
         vsk.popMatrix()
 
 
